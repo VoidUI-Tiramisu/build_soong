ae57ecb9969daabfd848dde78bf6844d45f80889
johnmart19 <johnivan19999@gmail.com>
2022-09-23 06:54:17 +0300

    Allow arm-linux-gnueabi "AS" and "LD" for kernel build tools
    Change-Id: Iaff3cb21e327abaa95ca060a4de09b1a211248f8

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -117,6 +117,8 @@ var Configuration = map[string]PathConfig{
 	"zip":     Allowed,
 	"arm-linux-androidkernel-as": Allowed,
 	"arm-linux-androidkernel-ld": Allowed,
+        "arm-linux-gnueabi-as": Allowed,
+        "arm-linux-gnueabi-ld": Allowed,
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
 	"ar":         Forbidden,
